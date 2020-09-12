e321ec0ec3c96fe7f2d5c8f52f2cfaa5696e695c
sinha104 <sinha104@umn.edu>
2020-09-12 18:45:33 +0530

    Try setting save un init cookie to true

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(session({
     //store: new FileStore,
     secret: "randomSecretKey",
     resave: false,
-    saveUninitialized: false,
+    saveUninitialized: true,
     cookie: {
         secure: true,
         sameSite: 'none'
