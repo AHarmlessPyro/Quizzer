c6db163405e0ea3538eaa739f86e14cd24de7219
sinha104 <sinha104@umn.edu>
2020-09-13 16:38:58 +0530

    fix table script mistakes

---
+++ build/precache-manifest.276dca526e72e6efd96d8b5e276d94d8 - Copy.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f2b935fe9d963aec346c1d46ed8ecc1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "010b0c35127d8a8f28c8",
+    "url": "./static/css/main.3aaba986.chunk.css"
+  },
+  {
+    "revision": "ca62f537a8e12af85851",
+    "url": "./static/js/2.fb9828b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.fb9828b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "010b0c35127d8a8f28c8",
+    "url": "./static/js/main.f48283db.chunk.js"
+  },
+  {
+    "revision": "d951622a8786f22a5d28",
+    "url": "./static/js/runtime-main.cb41e725.js"
+  },
+  {
+    "revision": "c22650c3aa49ae1b5c1ee1ae4b312792",
+    "url": "./static/media/logo_temp_1.c22650c3.png"
+  }
+]);
