999ab3086c9e59ae1d0d751672eb0ccf4cb99300
sinha104 <sinha104@umn.edu>
2020-09-15 23:07:25 +0530

    add some uncommited files

---
+++ build/precache-manifest.dcd7a6aa077ca7b9595a4176eed5e243.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45ed5632a015d22641b8ccf9e77c114c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "13907eb75ac30405fdff",
+    "url": "./static/css/main.03337362.chunk.css"
+  },
+  {
+    "revision": "e4a8688be5a485f5014d",
+    "url": "./static/js/2.4fe3264a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.4fe3264a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13907eb75ac30405fdff",
+    "url": "./static/js/main.9b4cdc85.chunk.js"
+  },
+  {
+    "revision": "d951622a8786f22a5d28",
+    "url": "./static/js/runtime-main.cb41e725.js"
+  },
+  {
+    "revision": "c22650c3aa49ae1b5c1ee1ae4b312792",
+    "url": "./static/media/logo_temp_1.c22650c3.png"
+  },
+  {
+    "revision": "3b8e0d8052682fc82928703f3178b590",
+    "url": "./static/media/share.3b8e0d80.png"
+  }
+]);
