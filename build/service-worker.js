e4b0204064e0cc1e7c67ab730325c40866041c46
sinha104 <sinha104@umn.edu>
2020-09-15 23:06:59 +0530

    change back getQuestion request to get from post

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.13dee1d48d33fa4ea5c8cbc393a44ee3.js"
+  "./precache-manifest.dcd7a6aa077ca7b9595a4176eed5e243.js"
 );
 
 self.addEventListener('message', (event) => {
