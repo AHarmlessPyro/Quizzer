42ebfb7db471f5304676acadac5170112d81254b
sinha104 <sinha104@umn.edu>
2020-09-16 00:43:41 +0530

    import missing imports

---
+++ build/precache-manifest.29a0e2ffab12083515b75662a967eb66.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "273f26d73ba4be8a6bbcad97f5810f2b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "878cae23ae58a56793ca",
+    "url": "./static/css/main.2e67a714.chunk.css"
+  },
+  {
+    "revision": "5dc2123878cf16c1b63f",
+    "url": "./static/js/2.e6bba352.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.e6bba352.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "878cae23ae58a56793ca",
+    "url": "./static/js/main.73b007bd.chunk.js"
+  },
+  {
+    "revision": "d951622a8786f22a5d28",
+    "url": "./static/js/runtime-main.cb41e725.js"
+  },
+  {
+    "revision": "c22650c3aa49ae1b5c1ee1ae4b312792",
+    "url": "./static/media/logo_temp_1.c22650c3.png"
+  },
+  {
+    "revision": "3b8e0d8052682fc82928703f3178b590",
+    "url": "./static/media/share.3b8e0d80.png"
+  }
+]);
