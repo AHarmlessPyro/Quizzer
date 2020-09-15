8b0257e540d89225563060b40a729b5eb1f834fa
sinha104 <sinha104@umn.edu>
2020-09-15 20:34:09 +0530

    add back in security settings

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ app.use(session({
     saveUninitialized: true,
     cookie: {
         maxAge: 14400000,
-        // secure: true,
-        // sameSite: 'none'
+        secure: true,
+        sameSite: 'none'
     }
 }))
 
